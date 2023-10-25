f64b13169756a7019585a838c22f1b6136a012b7
Dalto de Luca <103264114+dlt-vn@users.noreply.github.com>
2023-10-25 11:33:30 +0100

    Release 1.0
    Passagem de todo código para as boas normas de programação.

--- js/scripts.js
+++ js/scripts.js
@@ -1,131 +1,53 @@
-function sortearEquipes() {
-    const nomes = document.getElementById('nomes-equipes').value;
-    const nomesArray = nomes.split(',').map(nome => nome.trim());
+function raffleTeams() {
+    const names = document.getElementById('team-names').value;
+    const namesArray = names.split(',').map(name => name.trim());
 
-    if (nomesArray.length < 2) {
-        alert('Por favor, insira pelo menos dois nomes para formar as equipes.');
+    if (namesArray.length < 2) {
+        alert('Por favor, insira pelo menos dois names para formar as equipes.');
         return;
     }
 
-    const equipesSorteadas = formarEquipes(nomesArray);
-    document.getElementById('resultado-equipes').innerHTML = `Equipe 1: ${equipesSorteadas[0].join(', ')}<br>Equipe 2: ${equipesSorteadas[1].join(', ')}`;
+    const drawnTeams = formTeams(namesArray);
+    document.getElementById('team-results').innerHTML = `Equipe 1: ${drawnTeams[0].join(', ')} - <br>Equipe 2: ${drawnTeams[1].join(', ')} - `;
 }
 
-function formarEquipes(nomesArray) {
-    // Embaralhe a lista de nomes aleatoriamente
-    const nomesEmbaralhados = shuffleArray(nomesArray);
+function formTeams(namesArray) {
+    // sorteio de nomes
+    const randomNames = shuffleArray(namesArray);
 
-    const equipe1 = [];
-    const equipe2 = [];
+    const team1 = [];
+    const team2 = [];
 
-    for (let i = 0; i < nomesEmbaralhados.length; i++) {
+    for (let i = 0; i < randomNames.length; i++) {
         if (i % 2 === 0) {
-            equipe1.push(nomesEmbaralhados[i]);
+            team1.push(randomNames[i]);
         } else {
-            equipe2.push(nomesEmbaralhados[i]);
+            team2.push(randomNames[i]);
         }
     }
 
-    return [equipe1, equipe2];
+    return [team1, team2];
 }
 
-/* function sortearNomeUnico() {
-    const nomesLista = document.getElementById('nomes-unico').value;
-    const nomes = nomesLista.split(',').map(nome => nome.trim());
+// Ordem
+function raffleOrder() {
+    const namesList = document.getElementById('names-list').value;
+    const names = namesList.split(',').map(name => name.trim());
 
-    if (nomes.length === 0) {
-        alert('Por favor, insira pelo menos um nome.');
+    if (names.length < 2) {
+        alert('Por favor, insira pelo menos dois names.');
         return;
     }
 
-    const mapaSorteado = nomes[Math.floor(Math.random() * nomes.length)];
-    document.getElementById('resultado-nome-unico').textContent = `Mapa: ${mapaSorteado}`;
-} */
-const listaMapasBO3 = ["Aquarium", "Breach", "Combine", "Evac", "Exodus", "Fringe", "Hacoc", "Hunted", "Infection", "Metro", "Redwood", "Stronghold", "NUK3TOWN"];
-const listaMapasBO4 = ["Arsenal", "Contraband", "Firing Range", "Frequency", "Gridlock", "Hacienda", "Icebreaker", "Jungle", "Militia", "Morocco", "Payload", "Seaside", "Slums", "Summit"];
-const listaMapasCW = ["Armada", "Cartel", "Checkmate", "Crossroads", "Garrison", "Miami", "Moscow", "Satellite", "Nuketown", "Raid", "The Pines", "Express", "Apocalypse", "Miami Strike", "Diesel", "Yamantau", "Standoff", "Collateral", "Hijacked", "Rush", "Echelon", "Drive-In", "Slums", "Zoo", "Amerika", "Deprogram", "WMD", "Jungle"];
-
-function atualizarListaPredefinida() {
-    const select = document.getElementById('nomes-unico');
-    const selectedOption = select.value;
-
-    if (selectedOption === "other") {
-        document.getElementById('nome-unico-input').style.display = 'block';
-    } else {
-        document.getElementById('nome-unico-input').style.display = 'none';
-    }
-}
-
-function sortearNomePredefinido() {
-    const selectedOption = document.getElementById('nomes-unico').value;
-
-    if (selectedOption === "other") {
-        // Não faz nada aqui, o sorteio ocorrerá no botão "Sortear Nome Personalizado"
-        return;
-    }
-
-    let listaMapas = [];
-
-    switch (selectedOption) {
-        case "bo3":
-            listaMapas = listaMapasBO3;
-            break;
-        case "bo4":
-            listaMapas = listaMapasBO4;
-            break;
-        case "bocw":
-            listaMapas = listaMapasCW;
-            break;
-    }
-
-    if (listaMapas.length > 0) {
-        const indiceSorteado = Math.floor(Math.random() * listaMapas.length);
-        const mapaSorteado = listaMapas[indiceSorteado];
-        document.getElementById('resultado-nome-unico').textContent = `Mapa: ${mapaSorteado}`;
-    }
-}
-
-function sortearNomePersonalizado() {
-    const nomesLista = document.getElementById('nome-unico-input').value;
-    const nomes = nomesLista.split(',').map(nome => nome.trim());
-
-    if (nomes.length > 0) {
-        const indiceSorteado = Math.floor(Math.random() * nomes.length);
-        const mapaSorteado = nomes[indiceSorteado];
-        document.getElementById('resultado-nome-unico').textContent = `Mapa: ${mapaSorteado}`;
-    } else {
-        alert('Por favor, insira pelo menos um mapa separado por vírgula.');
-    }
-}
-
-// Função para embaralhar um array
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-// Função para exibir a ordem de nomes
-function sortearOrdemNomes() {
-    const nomesLista = document.getElementById('nomes-lista').value;
-    const nomes = nomesLista.split(',').map(nome => nome.trim());
-
-    if (nomes.length < 2) {
-        alert('Por favor, insira pelo menos dois nomes.');
-        return;
-    }
-
-    const nomesEmbaralhados = shuffleArray(nomes);
-    const ul = document.getElementById('resultado-ordem-nomes');
+    const randomNames = shuffleArray(names);
+    const ul = document.getElementById('order-result');
     ul.innerHTML = '';
 
-    for (let i = 0; i < nomesEmbaralhados.length; i++) {
+    for (let i = 0; i < randomNames.length; i++) {
         const li = document.createElement('li');
-        li.textContent = `${i + 1}º: ${nomesEmbaralhados[i]}`;
+        li.textContent = `${i + 1}º: ${randomNames[i]}`;
 
-        // Cria uma caixa de texto ao lado de cada nome
+        // Cria uma caixa de texto ao lado de cada name
         const input = document.createElement('input');
         input.type = 'text';
         input.placeholder = 'Digite algo';
@@ -135,31 +57,107 @@ function sortearOrdemNomes() {
     }
 }
 
+/* function sortearnameUnico() {
+    const namesLista = document.getElementById('map').value;
+    const names = namesLista.split(',').map(name => name.trim());
 
-function mostrarResultados() {
-    // Obtenha os resultados do sorteio de equipes
-    const resultadoEquipes = document.getElementById('resultado-equipes').textContent;
-
-    // Obtenha os resultados do sorteio de ordem de nomes
-    const ul = document.getElementById('resultado-ordem-nomes');
-    const resultadosOrdemNomes = [];
-    for (let i = 0; i < ul.children.length; i++) {
-        const nome = ul.children[i].textContent.split(': ')[1];
-        const textoCaixa = ul.children[i].getElementsByTagName('input')[0].value;
-        resultadosOrdemNomes.push({ nome, textoCaixa });
+    if (names.length === 0) {
+        alert('Por favor, insira pelo menos um name.');
+        return;
     }
 
-    // Obtenha o resultado do sorteio de nome único
-    const resultadoNomeUnico = document.getElementById('resultado-nome-unico').textContent;
+    const drawnMap = names[Math.floor(Math.random() * names.length)];
+    document.getElementById('map-result').textContent = `Mapa: ${drawnMap}`;
+} */
 
-    // Exiba os resultados onde você preferir na página
-    const resultadosDiv = document.getElementById('resultados');
-    resultadosDiv.innerHTML = '<h2>Resultados:</h2>';
-    resultadosDiv.innerHTML += `<p>Sorteio de Equipes: ${resultadoEquipes}</p>`;
-    resultadosOrdemNomes.forEach((resultado, index) => {
-        resultadosDiv.innerHTML += `<p>${resultado.nome} - Bans/Protects: ${resultado.textoCaixa}</p>`;
+const mapListBO3 = ["Aquarium", "Breach", "Combine", "Evac", "Exodus", "Fringe", "Hacoc", "Hunted", "Infection", "Metro", "Redwood", "Stronghold", "NUK3TOWN"];
+const mapListBO4 = ["Arsenal", "Contraband", "Firing Range", "Frequency", "Gridlock", "Hacienda", "Icebreaker", "Jungle", "Militia", "Morocco", "Payload", "Seaside", "Slums", "Summit"];
+const mapListCW = ["Armada", "Cartel", "Checkmate", "Crossroads", "Garrison", "Miami", "Moscow", "Satellite", "Nuketown", "Raid", "The Pines", "Express", "Apocalypse", "Miami Strike", "Diesel", "Yamantau", "Standoff", "Collateral", "Hijacked", "Rush", "Echelon", "Drive-In", "Slums", "Zoo", "Amerika", "Deprogram", "WMD", "Jungle"];
+
+function updateDefaultList() {
+    const select = document.getElementById('map');
+    const selectedOption = select.value;
+
+    if (selectedOption === "other") {
+        document.getElementById('map-input').style.display = 'block';
+    } else {
+        document.getElementById('map-input').style.display = 'none';
+    }
+}
+
+function raffleMap() {
+    const selectedOption = document.getElementById('map').value;
+
+    if (selectedOption === "other") {
+        return;
+    }
+
+    let mapList = [];
+
+    switch (selectedOption) {
+        case "bo3":
+            mapList = mapListBO3;
+            break;
+        case "bo4":
+            mapList = mapListBO4;
+            break;
+        case "bocw":
+            mapList = mapListCW;
+            break;
+    }
+
+    if (mapList.length > 0) {
+        const drawnIndex = Math.floor(Math.random() * mapList.length);
+        const drawnMap = mapList[drawnIndex];
+        document.getElementById('map-result').textContent = `Mapa: ${drawnMap}`;
+    }
+}
+
+function raffleCustomMap() {
+    const mapList = document.getElementById('map-input').value;
+    const map = mapList.split(',').map(name => name.trim());
+
+    if (map.length > 0) {
+        const drawnIndex = Math.floor(Math.random() * map.length);
+        const drawnMap = map[drawnIndex];
+        document.getElementById('map-result').textContent = `Mapa: ${drawnMap}`;
+    } else {
+        alert('Por favor, insira pelo menos um mapa separado por vírgula.');
+    }
+}
+
+// Sorteio
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function showResults() {
+    // Resultado Equipes
+    const teamResults = document.getElementById('team-results').textContent;
+
+    // Resultado Ordem
+    const ul = document.getElementById('order-result');
+    const orderResult = [];
+    for (let i = 0; i < ul.children.length; i++) {
+        const name = ul.children[i].textContent.split(': ')[1];
+        const textBox = ul.children[i].getElementsByTagName('input')[0].value;
+        orderResult.push({ name, textBox });
+    }
+
+    // Resultado Mapa
+    const mapResult = document.getElementById('map-result').textContent;
+
+    // Resultados
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '<h2>Resultados:</h2>';
+    resultsDiv.innerHTML += `<p>${teamResults}</p>`;
+    orderResult.forEach((result, index) => {
+        resultsDiv.innerHTML += `<p>${result.name} - Bans/Protects: ${result.textBox}</p>`;
     });
-
-    resultadosDiv.innerHTML += `<p>Sorteio de Nome Único: ${mapaSorteado}</p>`;
+    resultsDiv.innerHTML += `<p>Mapa: ${drawnMap}</p>`;
 }
 
